f3b6c9836d002bd4a4b237ca1789ff58f0c1fe76
Jorjeh <31190895+Jorjeh@users.noreply.github.com>
2021-11-14 19:01:51 -0400

    Fiunções ativas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+// interface TodoList {
+//   id: string;
+//   nome: string;
+//   done: boolean;
+// }
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
-
+    
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks(oldState => [...oldState, data])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updateTask = tasks.map((task => ({...task})));
+    const lookTask = updateTask.find(task => task.id === id);
+    
+    if (!lookTask){
+      return;
+    }
+
+    lookTask.done = !lookTask.done;
+    setTasks(updateTask)
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ));
   }
 
   return (
